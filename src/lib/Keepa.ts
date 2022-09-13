3760dca476738020201c7cb7ce03ce5a217fe97f
Ali <alimanssouri221@gmail.com>
2022-09-13 19:23:16 +0430

    Fix 59

--- src/lib/Keepa.ts
+++ src/lib/Keepa.ts
@@ -8,7 +8,7 @@ import {EnumCategories} from "../@types/EnumCategories";
 
 export default class Keepa {
     private input: KeepaInputType
-    private key = "1uuk3u3b55cvqn3fbq8ii90v44eeb138r0j5rvgcpjoove9vele1qqu33kmhglc7"
+    private key = "9lpb4evo4c87vkajmcnfft5dirt97imditg32d27kdp7pm0c861rqc44o6pv1tct"
     // private key = "42t2rfuagph4t942ccfi5l97lkkp5dh7ak0sm2brnc4a66nh4ouj82edn0pjp0on"
     private apiUrl = "https://api.keepa.com/"
     private domain: number = 1
