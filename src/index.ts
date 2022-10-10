8d4efe9dc554187a119c058b0dd72a60a8c8c379
Ali <alimanssouri221@gmail.com>
2022-10-10 23:37:51 +0330

    Fix 71

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,10 @@ if (process.env.NODE_ENV === 'development') {
         })
         .then(() => {
             console.log('Connection to database successfully')
-            app.listen(process.env.PORT, async () => {
-                console.log(`App is running on port ${process.env.PORT}`)
-            })
         })
         .catch((err) => {
             console.log('Connection to database failed: ' + err)
         })
-} else {
     app.listen(process.env.PORT, async () => {
         console.log(`App is running on port ${process.env.PORT}`)
     })
