36ce5293b9a8b6ac17b538d2b5d5be89d6dbd3d1
Ali <alimanssouri221@gmail.com>
2023-07-15 15:09:23 +0330

    Fix 274

--- src/test2.ts
+++ src/test2.ts
@@ -6,16 +6,6 @@ import DatabaseLeads from './sheets/DatabaseLeads'
 
 export async function main() {
     try {
-        const ff = new DatabaseLeads()
-        await ff.auth()
-        await ff.addToSheet({
-            source: {
-                url: 'http://localhost:8080',
-            },
-            amazon: {
-                asin: 'asdfghjuyt',
-            },
-        })
 
         /*await sourceCheck({
             url: 'https://www.tcgplayer.com/product/242808/pokemon-celebrations-celebrations-collection-lances-charizard-v?country=US&utm_campaign=9549531350&utm_source=google&utm_medium=cpc&utm_content=422888779645&utm_term=&adgroupid=103661046211&gclid=CjwKCAjwi8iXBhBeEiwAKbUofU68zOTx4dFPlFJjqFUibruaDG0Z13SjImGsE3GnLI6oh9Es5dOdSBoCPQkQAvD_BwE&Language=English',
